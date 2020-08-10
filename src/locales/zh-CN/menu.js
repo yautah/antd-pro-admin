ec61b52ee7e7b1567902070b0ea662681a901dea
yangquan <yangquan@mchina.cn>
2020-08-10 16:40:17 +0800

    no message

--- src/locales/zh-CN/menu.js
+++ src/locales/zh-CN/menu.js
@@ -49,4 +49,10 @@ export default {
   'menu.editor.flow': '流程编辑器',
   'menu.editor.mind': '脑图编辑器',
   'menu.editor.koni': '拓扑编辑器',
+
+
+  'menu.members': '记录列表',
+  'menu.records': '记录列表',
+  'menu.records.recordsNew': '新建记录',
+  'menu.records.recordsEdit': '编辑记录',
 };
